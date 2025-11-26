0d031143995a7346e83bebcc45ab7dde6d5631fd
Angelo <angelomaluya01@gmail.com>
2025-11-27 01:32:58 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { LoginSignup } from './Components/LogInSignup/LoginSignup';
-import { DashBoard } from './Components/Dashboard/DashBoard';
+import  DashBoard  from './Components/Dashboard/DashBoard';
 import { useState } from 'react';
 
 
 function App() {
-  const adminUser ={
-    username: "admin@admin.com",
-    password: "admin123"
-  }
-
-  const [user, setUser] = useState({name:"",username:""});
-  const [error, setError] = useState("");
-
-  const LogIn = details =>{
-    console.log(details);
-  }
-
-  const LogOut = details =>{
-    console.log("LogOutdetails");
-  }
-
   return (
-    <div>
-      <LoginSignup/>
-    </div>
+      <DashBoard/>
   );
 }
 
